502a5ccc819a0ede0a4a01c77a99755cf9494a9c
Bob <bob.tossaint@zuyd.nl>
2022-09-16 14:17:43 +0200

    RGB functionaliteit

--- main.py
+++ main.py
@@ -1,27 +1,70 @@
-import maqueen 
-
-def on_forever():
-    pass
-
-def turnleft(speed):
-    maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, speed)
-    maqueen.motor_stop(maqueen.Motors.M1)
+basic.show_icon(IconNames.HEART)
+strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
 
 def stop():
-    maqueen.motor_stop(maqueen.Motors.M1)   
-    maqueen.motor_stop(maqueen.Motors.M2) 
-
-def drive(speed):
-    maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, speed)
-    maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, speed)
+    maqueen.motor_stop(maqueen.Motors.M1)
+    maqueen.motor_stop(maqueen.Motors.M2)
 
 def sound():
     soundExpression.InterpolationEffect.ARPEGGIO_RISING_MAJOR
 
+def drive(speed: number):
+    maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, speed)
+    maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, speed)
+
+def turnleft(speed2: number):
+    maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, speed2)
+    maqueen.motor_stop(maqueen.Motors.M1)
+    
+def on_forever():
+    basic.show_number(maqueen.ultrasonic(PingUnit.CENTIMETERS))
+
+def on_forever2():
+    strip.show_color(neopixel.colors(NeoPixelColors.RED))
+    maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+    maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+    basic.pause(1000)
+    strip.show_color(neopixel.colors(NeoPixelColors.BLUE))
+    maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+    maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+    basic.pause(1000)
+    strip.show_color(neopixel.colors(NeoPixelColors.GREEN))
+    maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+    maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+    basic.pause(1000)
+    strip.show_color(neopixel.colors(NeoPixelColors.PURPLE))
+    maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CCW, 255)
+    maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CCW, 255)
+    basic.pause(1000)
+    strip.show_color(neopixel.colors(NeoPixelColors.YELLOW))
+    maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CCW, 255)
+    maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CCW, 0)
+    basic.pause(1000)
+
+
+def on_forever3():
+    maqueen.write_led(maqueen.LED.LED_LEFT, maqueen.LEDswitch.TURN_OFF)
+    maqueen.write_led(maqueen.LED.LED_RIGHT, maqueen.LEDswitch.TURN_ON)
+    music.play_tone(494, music.beat(BeatFraction.WHOLE))
+    basic.pause(500)
+    maqueen.write_led(maqueen.LED.LED_LEFT, maqueen.LEDswitch.TURN_ON)
+    maqueen.write_led(maqueen.LED.LED_RIGHT, maqueen.LEDswitch.TURN_OFF)
+    music.play_tone(262, music.beat(BeatFraction.WHOLE))
+    basic.pause(500)
+
 while True:
-    drive(200)    
-    while maqueen.ultrasonic(0) < 20 :
-        music.play_sound_effect(music.create_sound_effect(WaveShape.SINE, 5000, 0, 255, 0, 500, SoundExpressionEffect.NONE, InterpolationCurve.LINEAR), SoundExpressionPlayMode.UNTIL_DONE)
-        turnleft(200)         
-            
-            
+    drive(200)
+    while maqueen.ultrasonic(0) < 20:
+        music.play_sound_effect(music.create_sound_effect(WaveShape.SINE,
+                5000,
+                0,
+                255,
+                0,
+                500,
+                SoundExpressionEffect.NONE,
+                InterpolationCurve.LINEAR),
+            SoundExpressionPlayMode.UNTIL_DONE)
+        turnleft(200)
+    basic.forever(on_forever)
+    basic.forever(on_forever2)
+    basic.forever(on_forever3)
--- main.ts
+++ main.ts
@@ -1,15 +1,12 @@
-function on_forever() {
-    
-}
-
-function turnleft(speed: number) {
-    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, speed)
-    maqueen.motorStop(maqueen.Motors.M1)
-}
-
+basic.showIcon(IconNames.Heart)
+let strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
 function stop() {
     maqueen.motorStop(maqueen.Motors.M1)
     maqueen.motorStop(maqueen.Motors.M2)
+}
+
+function sound() {
+    soundExpression.InterpolationEffect.ArpeggioRisingMajor
 }
 
 function drive(speed: number) {
@@ -17,8 +14,9 @@ function drive(speed: number) {
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, speed)
 }
 
-function sound() {
-    soundExpression.InterpolationEffect.ArpeggioRisingMajor
+function turnleft(speed2: number) {
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, speed2)
+    maqueen.motorStop(maqueen.Motors.M1)
 }
 
 while (true) {
@@ -27,4 +25,39 @@ while (true) {
         music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
         turnleft(200)
     }
+    basic.forever(function on_forever() {
+        basic.showNumber(maqueen.Ultrasonic(PingUnit.Centimeters))
+    })
+    basic.forever(function on_forever2() {
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+        basic.pause(1000)
+        strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+        basic.pause(1000)
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+        basic.pause(1000)
+        strip.showColor(neopixel.colors(NeoPixelColors.Purple))
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 255)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 255)
+        basic.pause(1000)
+        strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 255)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 0)
+        basic.pause(1000)
+    })
+    basic.forever(function on_forever3() {
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+        music.playTone(494, music.beat(BeatFraction.Whole))
+        basic.pause(500)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+        music.playTone(262, music.beat(BeatFraction.Whole))
+        basic.pause(500)
+    })
 }
